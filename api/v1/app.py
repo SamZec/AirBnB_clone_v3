ad67d7f20d599193392b06e4195abf9d63162e1f
SamZec <samzec4@gmail.com>
2023-07-31 23:29:53 +0000

    3. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
+"""runs flask server"""
+
 from flask import Flask, jsonify
-
-app = Flask(__name__)
-
 from models import storage
 from api.v1.views import app_views
 
+
+app = Flask(__name__)
 app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_db(Exception):
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
     """returns a JSON-formatted 404 status code response"""
     return jsonify({'error': 'not found'})
 
+
 @app.errorhandler(400)
 def request_body_error(error):
     """return json formatted 400 status coode"""
     return jsonify({'400': error.description})
+
 
 port = 5000
 host = '0.0.0.0'
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""an endpoint that retrieves the number of each objects by type"""
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route("/status")
 def status():
+    """returns a JSON: "status": "OK" """
     return jsonify({'status': 'ok'})
+
 
 @app_views.route("/stats")
 def get_stats():
