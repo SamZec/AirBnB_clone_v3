8c72a1d57701ae704d507f73948177b085191cfb
Jasemonu <jasemonu@gmail.com>
2023-07-31 20:37:31 +0000

    db_storage.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import jsonify
+#from models import storage
+
+# Create a route /status on the app_views Blueprint
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
+
+"""
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    
+    Retrieve the number of each object by type.
+
+    Returns:
+        JSON response with the count of each object type.
+    
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+     }
+
+    return jsonify(stats)
+
+ """   
+
